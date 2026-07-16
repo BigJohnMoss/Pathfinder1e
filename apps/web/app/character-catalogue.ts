0f28b9e34cda60c5a263ac8697e1258455d96c41
BigJohnMoss <kgfm201@gmail.com>
2026-07-16 04:16:09 +0100

    Add shield to arcanist spellbook

--- apps/web/app/character-catalogue.ts
+++ apps/web/app/character-catalogue.ts
@@ -10,10 +10,11 @@ import ragePowers from "../../../packages/data/src/options/rage-powers.json";
 import rogueTalents from "../../../packages/data/src/options/rogue-talents.json";
 import mageArmor from "../../../packages/data/src/spells/mage-armor.json";
 import magicMissile from "../../../packages/data/src/spells/magic-missile.json";
+import shield from "../../../packages/data/src/spells/shield.json";
 
 export const classes = [arcanist, barbarian, fighter, rogue];
 export { human };
 export const feats = [combatCasting, powerAttack];
 export const optionGroups = [exploits, ragePowers, rogueTalents];
-export const spells = [mageArmor, magicMissile];
+export const spells = [mageArmor, magicMissile, shield];
 export const skills = [{name:"Acrobatics",ability:"dexterity"},{name:"Climb",ability:"strength"},{name:"Diplomacy",ability:"charisma"},{name:"Knowledge (arcana)",ability:"intelligence"},{name:"Perception",ability:"wisdom"},{name:"Spellcraft",ability:"intelligence"},{name:"Stealth",ability:"dexterity"}] as const;
