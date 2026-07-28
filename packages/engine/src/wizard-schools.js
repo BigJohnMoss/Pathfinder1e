a145d52af6e96f1356f9da0fd3c03af7ef25b02a
Kevin <kgfm201@gmail.com>
2026-07-28 23:22:09 +0100

    Keep elemental schools out of Core opposition choices

--- packages/engine/src/wizard-schools.js
+++ packages/engine/src/wizard-schools.js
@@ -6,5 +6,9 @@ export function oppositionSchoolOptions(options, selectedSchool) {
   if (selectedSchool?.elementalOppositionSchool) {
     return options.filter((option) => schoolName(option.id, "wizard-opposition-") === selectedSchool.elementalOppositionSchool);
   }
-  return options.filter((option) => schoolName(option.id, "wizard-opposition-") !== school);
+  const elementalSchools = new Set(["air", "earth", "fire", "water"]);
+  return options.filter((option) => {
+    const optionSchool = schoolName(option.id, "wizard-opposition-");
+    return optionSchool !== school && !elementalSchools.has(optionSchool);
+  });
 }
